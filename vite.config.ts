ba6f741b30eb9a5ab7efb7fb9f581697a0b5e78a
Roy.Xu <bbb233456@gmail.com>
2026-09-13 00:24:07 +0800

    Add allow hosts

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
+    allowedHosts: [
+      "turtlesoupfrontend.onrender.com",
+    ],
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
